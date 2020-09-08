9cf2887ccaed4f55211653fcd796c91a55175518
haseebuchiha <haseeb9009@gmail.com>
2020-09-08 06:48:39 +0500

    Add project files.

---
+++ Assets/Scenes/Materials/changematerial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changematerial : MonoBehaviour {
+
+    public Material oldMat;
+    public Material newMat;
+    
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material = oldMat;
+        Invoke("m1", 1.0f);
+	}
+
+    void m1()
+    {
+        GetComponent<Renderer>().material = oldMat;
+        Invoke("m2", 1.0f);
+
+    }
+    
+    void m2()
+    {
+        GetComponent<Renderer>().material = newMat;
+        Invoke("m1", 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
